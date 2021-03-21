2591daeb7f2aa95664f7c8f5b51751536716dff0
alext <rumych2013@gmail.com>
2021-03-21 16:38:50 +0200

    start-1

--- newds/settings.py
+++ newds/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,12 +73,18 @@ WSGI_APPLICATION = 'newds.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+POSTGRES_DB = os.getenv('POSSTGRES_DB', 'dsstorage')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '1234567')
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DB,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': POSTGRES_HOST,
+    },
 }
 
 
