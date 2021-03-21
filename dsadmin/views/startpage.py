53aba3b2e63950fc5845a87832878d0c39a0455b
alext <rumych2013@gmail.com>
2021-03-21 18:46:39 +0200

    start-templates-1

--- dsadmin/views/startpage.py
+++ dsadmin/views/startpage.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 from dsadmin.views.basepage import BaseAdminView
 
 class MainView(BaseAdminView, TemplateView):
-    template_name = 'starter.html'
+    template_name = 'index.html'
